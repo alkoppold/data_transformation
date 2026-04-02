d092ddb2a60cb673463a732a9a5961c1a4e44250
m-klingelhoefer-j <m.klingelhoefer-jens@uke.de>
2026-04-02 11:13:52 +0200

    Added notes to cleanup and made the results scripts run again

--- scripts/extract_cleanup.R
+++ scripts/extract_cleanup.R
@@ -273,7 +273,11 @@ data_extract %>% filter(design %>% is.na() | design == "not reported") %>% selec
 data_extract %>% checkContent(homoscedasticity)
 data_extract %>% filter(design != "within") %>% checkContent(homoscedasticity)
 
-#TODO Maren: check in data set
+### Maren: check in data set
+# Case 1 (https://doi.org/10.1037/xge0000551): they used a paired t-test for the main effect of task but may have checked the homoscedasticity for other tests? 
+# But could not find unpaired tests... -> will be included in the discussion
+# Case 2 (https://doi.org/10.1016/j.biopsych.2008.09.011): needs to be checked -> is there a main effect of task?
+# Case 3 (https://doi.org/10.1080/02699931.2016.1158695): as case 1
 data_extract %>% filter(design == "within", homoscedasticity != "not reported") %>% select(doi, design, starts_with("homoscedasticity"))
 
 # * * * Homoscedasticity How ----------------------------------------------
